30b855f13ff8f1ffb4c4fc63e48f6d8d46969a9d
Jhovan <jhovan.gallardo@gmail.com>
2024-05-27 03:07:28 -0700

    Rename /create-lead/ to /leads/create

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/create-lead/", response_model=schemas.Lead)
+
+@app.post("/leads/create", response_model=schemas.Lead)
 def create_lead(db: Session = Depends(get_db)):
     return crud.create_lead(db=db)
+
 
 @app.get("/leads/", response_model=list[schemas.Lead])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
